a459c52bd44f3da987c464f745a379ded2778a35
Raymond <coderpeasant.wang@gmail.com>
2018-05-22 23:46:24 +0800

    commit

---
+++ corelib/src/main/java/com/ray/corelib/app/ConfigType.java
@@ -0,0 +1,11 @@
+package com.ray.corelib.app;
+
+/**
+ * Created by Raymond on 2018/5/22 23:37
+ */
+public enum ConfigType {
+    API_HOST,
+    APPLICATION_CONTEXT,
+    CONFIG_READY,
+    ICON
+}
--- corelib/src/main/java/com/ray/corelib/app/Configurator.java
+++ corelib/src/main/java/com/ray/corelib/app/Configurator.java
@@ -5,4 +5,14 @@ import java.util.WeakHashMap;
 public class Configurator {
     //据说WeakHashMap的键值对在不适用时就会回收
     private static final WeakHashMap<String ,Object> IMALL_CONFIGS=new WeakHashMap<>();
+    private Configurator(){
+        IMALL_CONFIGS.put(ConfigType.CONFIG_READY.name(),false);
+    }
+
+    /**
+     * 静态内部类单例
+     */
+    private static class Holder{
+        private static final Configurator INSTANCE=new Configurator();
+    }
 }
